eadb8352062eadc7b76698c7220e0d9d568eca20
ankd-k <ankd.2653@gmail.com>
2019-05-21 06:08:39 +0900

    add debug div

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnecti
 var RTCSessionDescription = window.RTCSessionDescription || window.webkitRTCSessionDescription || window.mozRTCSessionDescription;
 var RTCIceCandidate = window.RTCIceCandidate || window.webkitRTCIceCandidate || window.mozRTCIceCandidate;
 
+var debugDOM = window.getElementById('debug');
+debugDOM.innerText = 'debug';
+
 // フロント（イン）カメラの場合
 // const medias = {audio : false, video : { facingMode: "user" }},
 //       video  = document.getElementById("video");
@@ -15,9 +18,11 @@ const video  = document.getElementById("video");
 navigator.getUserMedia(medias, successCallback, errorCallback);
 
 function successCallback(stream) {
+  debugDOM.innerText = 'success.';
   video.srcObject = stream;
 };
 
 function errorCallback(error) {
-  alert(error);
+  // alert(error);
+  debugDOM.innerText = error;
 };
