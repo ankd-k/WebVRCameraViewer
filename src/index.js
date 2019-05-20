4a936866303d5c1a8bb1e4aae49b66155d43906e
ankd-k <ankd.2653@gmail.com>
2019-05-21 05:35:42 +0900

    wbertc

---
+++ src/index.js
@@ -0,0 +1,23 @@
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+var URL = window.URL || window.webkitURL;
+var RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+var RTCSessionDescription = window.RTCSessionDescription || window.webkitRTCSessionDescription || window.mozRTCSessionDescription;
+var RTCIceCandidate = window.RTCIceCandidate || window.webkitRTCIceCandidate || window.mozRTCIceCandidate;
+
+// フロント（イン）カメラの場合
+const medias = {audio : false, video : { facingMode: "user" }},
+      video  = document.getElementById("video");
+
+// リア（バック/アウト）カメラの場合
+const medias = {audio : false, video : { facingMode: { exact: "environment" } }};
+const video  = document.getElementById("video");
+
+navigator.getUserMedia(medias, successCallback, errorCallback);
+
+function successCallback(stream) {
+  video.srcObject = stream;
+};
+
+function errorCallback(error) {
+  alert(error);
+};
